d66bb65815ca691459a331d6ffc14d61a6f579a7
James Alhassan <jamesalhassan052@gmail.com>
2022-12-15 06:25:06 +0000

    main.h updated

--- main.h
+++ main.h
@@ -66,7 +66,7 @@ ssize_t getstdin(char **lineptr, int file);
 char  *clean_line(char *content);
 void f_push(stack_t **head, unsigned int number);
 void f_pall(stack_t **head, unsigned int number);
-
+void f_pint(stack_t **head, unsigned int number);
 
 
 #endif
