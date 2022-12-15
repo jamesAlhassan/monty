e928bd11a9b201cfdafdb7f4608629cd6972bfba
James Alhassan <jamesalhassan052@gmail.com>
2022-12-15 21:09:36 +0000

    f_pall func added

---
+++ operator4.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* f_pall - function that prints everything in the stack
+* @head: double head pointer to the stack
+* @counter: unused line count
+*
+* Return: nothing
+*/
+void f_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
+
