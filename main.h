0c7a8a80f5491a35edc2a264a7282fe9da9fe7ae
James Alhassan <jamesalhassan052@gmail.com>
2022-12-15 06:24:13 +0000

    main.h updated

--- main.h
+++ main.h
@@ -64,7 +64,7 @@ extern bus_t bus;
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
 char  *clean_line(char *content);
-
+void f_push(stack_t **head, unsigned int number);
 
 
 
