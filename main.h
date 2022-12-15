e0be5fae708165cd1db80ad8cf14bd07a5071fd9
James Alhassan <jamesalhassan052@gmail.com>
2022-12-15 20:55:09 +0000

    main.h updated

--- main.h
+++ main.h
@@ -68,6 +68,11 @@ void f_push(stack_t **head, unsigned int number);
 void f_pall(stack_t **head, unsigned int number);
 void f_pint(stack_t **head, unsigned int number);
 int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
+void free_stack(stack_t *head);
+void f_pop(stack_t **head, unsigned int counter);
+void f_swap(stack_t **head, unsigned int counter);
+
+
+
 
 #endif
-
