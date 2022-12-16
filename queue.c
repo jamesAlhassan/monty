0fee634eeb3c228a9d6d0129163eb7e9f5160617
James Alhassan <jamesalhassan052@gmail.com>
2022-12-16 05:28:11 +0000

    f_queue added

---
+++ queue.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* f_queue - function that prints the top of the queue
+* @head: head of queue
+* @counter: line count
+*
+* Return: nothing
+*/
+void f_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
